4829447b9ffadfb3502bf798986e0ea8b93f34d4
xuning <xningdd@163.com>
2023-07-30 12:45:30 +0800

    正则预编译&测试校验&ctx.Bind

---
+++ webbook/internal/web/user.go
@@ -0,0 +1,94 @@
+package web
+
+import (
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	// 校验邮箱格式的正则表达式
+	emailRegexPattern = "^[\\w\\.-]+@[a-zA-Z\\d\\.-]+\\.[a-zA-Z]{2,}$"
+	// 校验密码格式的正则表达式
+	passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+)
+
+// UserHandler 定义所有跟用户有关的路由
+// 为了分组方便， 为了依赖注入
+type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	// 将正则表达式的预编译放到 UserHandler的初始化中
+	return &UserHandler{
+		emailExp:    regexp.MustCompile(emailRegexPattern, regexp.None),
+		passwordExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
+	}
+}
+
+// SignUp 注册用户信息
+func (u *UserHandler) SignUp(ctx *gin.Context) {
+
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"Password"`
+	}
+
+	var req SignUpReq
+	// Bind 方法会根据 Content-Type 来解析你的数据到 req 里边
+	// 解析错了， 就会直接会写一个 400 的错误
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	isEmail, err := u.emailExp.MatchString(req.Email)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !isEmail {
+		ctx.String(http.StatusOK, "邮箱格式错误")
+		return
+	}
+
+	if req.Password != req.ConfirmPassword {
+		ctx.String(http.StatusOK, "两次输入的密码不相同")
+		return
+	}
+
+	isPassword, err := u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !isPassword {
+		ctx.String(http.StatusOK, "密码必须包含数字、特殊字符，并且长度不能小于 8 位")
+		return
+	}
+}
+
+// Login 用户登录
+func (u *UserHandler) Login(ctx *gin.Context) {
+
+}
+
+// Edit 用户这是信息
+func (u *UserHandler) Edit(ctx *gin.Context) {
+
+}
+
+// Profile 返回所有用户信息
+func (u *UserHandler) Profile(ctx *gin.Context) {
+
+}
+
+func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
+	ug := server.Group("/users")
+	ug.POST("/signup", u.SignUp)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
+	ug.GET("/profile", u.Profile)
+}
